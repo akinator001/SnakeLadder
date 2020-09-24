831914593ffeba39e9448ff6be22ac1b0809c00c
akinator001 <aakashgandhi1279@gmail.com>
2020-09-24 13:10:34 +0530

    (aakash) inital commit

---
+++ SnakeAndLadder.java
@@ -0,0 +1,7 @@
+
+public class SnakeAndLadder {
+	public static void main(String[] args){
+		System.out.println("Welcome to the game...");
+		System.out.println("please wash your hand before playing..");
+	}
+}
