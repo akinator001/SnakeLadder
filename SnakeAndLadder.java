78c03abed3823e420513c93fb2b852bf4e1c2a2b
akinator001 <aakashgandhi1279@gmail.com>
2020-09-24 13:41:11 +0530

    (aakash) modified uc4 with its function

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,28 +2,35 @@ import java.util.Random;
 public class SnakeAndLadder {
 	public static void main(String[] args){
 		
-		Random rnd = new Random();
+        Random rnd = new Random();
 		
-		int noplay = 0 ; 
+        int noplay = 0 ;
 		int ladder = 1 ;
-		int snake = 2 ;
-		
+		int snake = 2 ; 
 		int pos = 0 ;
-		int dice = rnd.nextInt(6) + 1 ;
-		System.out.println("Number on dice = " + dice);
-		int play = rnd.nextInt(3) ;
 		
-		if(play != noplay) {
-			if(play==ladder)
-				pos += dice;
+		//System.out.println("Number on dice = "+dice);
+		while(pos < 100)
+		{
+			int dice = rnd.nextInt(6) + 1 ;
+			int play = rnd.nextInt(3) ;
 			
-			else if(play == snake)
-				pos -= dice;
-			
-			System.out.println("New position = " + pos);
+			if(play != noplay) {
+				if(play==ladder)
+				{
+					pos += dice;
+				}
+				
+				else if(play == snake)
+				{
+					pos -= dice;
+					if(pos<0)
+						pos = 0;
+				}
+				
+				System.out.println("New position = "+pos);
+			}
 		}
-		else 
-			System.out.println("No play");
-
+		System.out.println("The player reached the position 100 and won");
 	}
 }
