70e62f1eb6258900e202237e1e7d940be487da45
akinator001 <aakashgandhi1279@gmail.com>
2020-09-24 13:19:18 +0530

    (aakash) uc1 task added , completed uc1

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,5 +3,8 @@ public class SnakeAndLadder {
 	public static void main(String[] args){
 		System.out.println("Welcome to the game...");
 		System.out.println("please wash your hand before playing..");
+			
+		int pos = 0 ; 
+		System.out.println("Single player at position " + pos);
 	}
 }
