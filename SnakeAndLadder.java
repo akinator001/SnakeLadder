08357df99f84d90ed87690b9b51e53de74728ce8
akinator001 <aakashgandhi1279@gmail.com>
2020-09-24 13:26:53 +0530

    (aakash) modified uc2 with its function

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,11 @@
-
+import java.util.Random;
 public class SnakeAndLadder {
 	public static void main(String[] args){
-		System.out.println("Welcome to the game...");
-		System.out.println("please wash your hand before playing..");
-			
-		int pos = 0 ; 
-		System.out.println("Single player at position " + pos);
+		
+		Random rnd = new Random();
+		int dice = rnd.nextInt(6) + 1;
+		
+		System.out.println("Number on dice = "+dice);
+
 	}
 }
