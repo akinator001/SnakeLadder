c1383cf3b300c6fda4051eaad000a755f043750c
akinator001 <aakashgandhi1279@gmail.com>
2020-09-24 13:34:54 +0530

    (aakash) modified uc3 with its function

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,9 +3,27 @@ public class SnakeAndLadder {
 	public static void main(String[] args){
 		
 		Random rnd = new Random();
-		int dice = rnd.nextInt(6) + 1;
 		
-		System.out.println("Number on dice = "+dice);
+		int noplay = 0 ; 
+		int ladder = 1 ;
+		int snake = 2 ;
+		
+		int pos = 0 ;
+		int dice = rnd.nextInt(6) + 1 ;
+		System.out.println("Number on dice = " + dice);
+		int play = rnd.nextInt(3) ;
+		
+		if(play != noplay) {
+			if(play==ladder)
+				pos += dice;
+			
+			else if(play == snake)
+				pos -= dice;
+			
+			System.out.println("New position = " + pos);
+		}
+		else 
+			System.out.println("No play");
 
 	}
 }
